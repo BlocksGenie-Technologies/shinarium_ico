0336f0744932304302a0f66a58d7a24555ec449c
Daniel Padrino <danswarrior1@gmail.com>
2022-01-17 17:32:01 -0300

    basic styling for inputs

--- src/components/TokenInput/index.jsx
+++ src/components/TokenInput/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import styles from "./styles.module.css";
 
 const TokenInput = () => {
   return (
-    <div>
-      <label htmlFor="token">Token</label>
-      <input type="number" id="token" />
+    <div className={styles.inputWrapper}>
+      <input
+        id="token"
+        className={styles.input}
+        inputmode="decimal"
+        autocomplete="off"
+        autocorrect="off"
+        type="text"
+        pattern="^[0-9]*[.,]?[0-9]*$"
+        placeholder="0.0"
+        minlength="1"
+        maxlength="79"
+        spellcheck="false"
+      />
+      <button className={styles.changeAsset}>
+        <img
+          width={25}
+          src="https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+          alt=""
+        />
+        <span>ETH</span>
+        <span>▼</span>
+      </button>
     </div>
   );
 };
