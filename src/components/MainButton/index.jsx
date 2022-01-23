0458002f1f3cc3cd4eaee9214f40237462f67e3f
Daniel Padrino <danswarrior1@gmail.com>
2022-01-22 23:26:29 -0300

    web3 layer

--- src/components/MainButton/index.jsx
+++ src/components/MainButton/index.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useWeb3React } from "@web3-react/core";
+
+import useAuth from "../../hooks/useAuth";
 import styles from "./styles.module.css";
 
 const MainButton = () => {
+  const { login, logout } = useAuth();
+  const { account } = useWeb3React();
+
   return (
-    <button className={styles.mainButton} onClick={(e) => e.preventDefault()}>
-      Connect wallet
+    <button className={styles.mainButton} onClick={account ? logout : login}>
+      {account || "Connect wallet"}
     </button>
   );
 };
--- src/view/Swap/index.jsx
+++ src/view/Swap/index.jsx
@@ -1,22 +1,35 @@
 import React from "react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 
 import Header from "../../components/Header";
 import TokenInput from "../../components/TokenInput";
 import InterchangeButton from "../../components/InterchangeButton";
 import MainButton from "../../components/MainButton";
-import styles from "./styles.module.css";
 import LateralPanelProvider from "../../providers/LateralPanelProvider";
+import AuthProvider from "../../providers/AuthProvider";
+import styles from "./styles.module.css";
+
+const getLibrary = (provider) => {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+};
 
 const Swap = () => {
   return (
     <div className={styles.swapContainer}>
-      <LateralPanelProvider>
-        <Header />
-        <TokenInput />
-        <InterchangeButton />
-        <TokenInput />
-        <MainButton />
-      </LateralPanelProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <AuthProvider>
+          <LateralPanelProvider>
+            <Header />
+            <TokenInput />
+            <InterchangeButton />
+            <TokenInput />
+            <MainButton />
+          </LateralPanelProvider>
+        </AuthProvider>
+      </Web3ReactProvider>
     </div>
   );
 };
