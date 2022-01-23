aff0f476eb8b2c4cae2cc2791383d21d5943e584
Daniel Padrino <danswarrior1@gmail.com>
2022-01-23 16:30:58 -0300

    logout button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useWeb3React } from "@web3-react/core";
 
+import useAuth from "../../hooks/useAuth";
 import useLateralPanel from "../../hooks/useLateralPanel";
 import Settings from "../Settings";
 import styles from "./styles.module.css";
 
 const Header = () => {
+  const { logout } = useAuth();
+  const { account } = useWeb3React();
+
   const [openPanel] = useLateralPanel({
     title: "Settings",
     content: <Settings />
@@ -13,9 +18,17 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Swap</h1>
-      <button className={styles.iconButton} onClick={() => openPanel()}>
-        <i className="fas fa-cog"></i>
-      </button>
+      <div>
+        <button className={styles.iconButton} onClick={() => openPanel()}>
+          <i className="fas fa-cog"></i>
+        </button>
+        <button
+          className={`${styles.iconButton} ${!account && styles.iconExitHide}`}
+          onClick={logout}
+        >
+          <i class="fas fa-sign-out-alt"></i>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/MainButton/index.jsx
+++ src/components/MainButton/index.jsx
@@ -1,16 +1,14 @@
-import React, { useEffect } from "react";
-import { useWeb3React } from "@web3-react/core";
+import React from "react";
 
 import useAuth from "../../hooks/useAuth";
 import styles from "./styles.module.css";
 
 const MainButton = () => {
-  const { login, logout } = useAuth();
-  const { account } = useWeb3React();
+  const { login } = useAuth();
 
   return (
-    <button className={styles.mainButton} onClick={account ? logout : login}>
-      {account || "Connect wallet"}
+    <button className={styles.mainButton} onClick={login}>
+      Connect wallet
     </button>
   );
 };
