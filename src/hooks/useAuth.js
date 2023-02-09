85aabc3942bb46d5fb16433cab1d3f8697e95334
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2023-02-09 13:42:20 +1300

    update

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,6 @@
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
+import { Web3Provider } from '@ethersproject/providers';
 
 const injected = new InjectedConnector();
 
